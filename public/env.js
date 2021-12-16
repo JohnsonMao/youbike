329635943cd7ad5ee42b5ca2cbc29078c4872a2b
JohnsonMao <86179381+JohnsonMao@users.noreply.github.com>
2021-12-16 21:58:08 +0800

    fix 佈署調整

--- public/env.js
+++
@@ -1,7 +0,0 @@
-window.env = {
-  "APP_ID": "01a52d6dfedd46fd977fdca580d5d367",
-  "APP_KEY": "phBeE27SlRYhx7ipzjIoTpM19Ys",
-  "MAP_TOKEN": "pk.eyJ1IjoidHV0ZWxhcnltYW8iLCJhIjoiY2t3MXUxdW81MXBkazJ2cWlicm55dzZoaSJ9.jp081D0yv__D8iUT8i31aQ",
-  "MAP_USERNAME": "tutelarymao",
-  "MAP_STYLE_ID": "ckw1tplwo0egd14qti5bgzng0"
-};
